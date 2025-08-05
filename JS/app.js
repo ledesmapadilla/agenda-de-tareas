844c73a80c1ed6603962caa90ef1da5e622b1666
ignaciolp <ignaciolp@lepa.com.ar>
2025-08-04 23:07:15 -0300

    termino de colocar la fecha de edicion y el cambio entre los estados

--- JS/app.js
+++ JS/app.js
@@ -4,10 +4,21 @@ const btnAgregarTarea = document.getElementById("btnAgregarTarea");
 const modalFormularioTarea = new bootstrap.Modal(
   document.getElementById("tareaModal")
 );
+const tablaDetalle = document.getElementById("seccionDetalleTarea");
+const btnVolverLista = document.getElementById("btnVolverLista");
+const detalleTareaTitulo = document.getElementById("detalleTarea");
+const detalleDescripcion = document.getElementById("detalleDescripcion");
+const detalleResponsable = document.getElementById("detalleResponsable");
+const detalleEstado = document.getElementById("detalleEstado");
+const detalleFechaEdit = document.getElementById("detalleFechaEdic");
+/* const detalleFechaModif = document.getElementById("detalleFechaModif"); */
+const detalleFechaCrea = document.getElementById("detalleFechaCreacion");
 const formularioTarea = document.getElementById("formTarea");
 const inputTarea = document.getElementById("tarea");
 const inputResponsable = document.getElementById("responsable");
 const inputUrgencia = document.getElementById("urgencia");
+let estoyCreando = true;
+let idTarea = null;
 
 /* poner fecha actual con formato */
 const fechaActual = new Date();
@@ -16,18 +27,19 @@ const opcionesFecha = {
   day: "numeric",
   month: "long",
   year: "numeric",
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
 };
 const fechaFormateada = fechaActual.toLocaleDateString(
   undefined,
   opcionesFecha
 );
+/* voy a traer el tbody para hacer tabla */
+const tbody = document.querySelector(`#tablaTareasBody`);
 
 /* cuando hago F5, que no me borre el array de la agenda */
 const agenda = JSON.parse(localStorage.getItem("agendaKey")) || [];
-
-/* const tablaTareas = document.getElementById("tablaTarea"); */
-
-/* const agenda = []; */
 
 //Funciones
 const guardarLocalstorage = () => {
@@ -38,10 +50,11 @@ const crearTarea = () => {
   //buscar los datos del formulario y crear un objeto tarea
   const tareaNueva = new Tarea(
     inputTarea.value,
-    fechaFormateada,
-    null,
+    `original`,
     inputResponsable.value,
-    inputUrgencia.value
+    inputUrgencia.value,
+    fechaFormateada,
+    null
   );
   //guardar el contacto en la agenda de tareas
   agenda.push(tareaNueva);
@@ -54,50 +67,216 @@ const crearTarea = () => {
     confirmButtonText: "OK",
   });
 
+  /* dibujar la tarea en la tabla */
+  dibujarFila(tareaNueva, agenda.length);
+
   //guardar la agenda en el localstorage
   guardarLocalstorage();
 };
 
-/* insertar los datos en la tabla */
-/*  const tareasTabla = document.createElement("tbody"); */
+const editarTarea = () => {
+  const indiceTarea = agenda.findIndex((tarea) => tarea.id == idTarea);
+  /* creo fecha de edicion */
 
-/*  tareasTabla.innerHTML = `
-              <td>${inputTarea.value}</td>
-              <td>${inputFechaCrea.value}</td>
-              <td>${inputHoraCrea.value}</td>
-              <td>${inputEstado.value}</td>
-              <td>
-                <button type="button" class="btn btn-primary me-1 btn-eliminar">
-                  Eliminar
-                </button>
-                <button type="button" class="btn btn-success me-1 btn-editar">
-                  Editar
-                </button>
-                <button type="button" class="btn btn-secondary btn-finalizar">
-                  Finalizar
-                </button>
-              </td>
-              
-            `; */
+  const fechaEditada = new Date();
+  const opcionesFechaEditada = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  };
 
-/* borrar tarea */
-/* const btnBorraTarea = tareasTabla.querySelector(".btn-primary"); 
-  
-  btnBorraTarea.addEventListener("click", () => {
-    tareasTabla.remove();
-  });  */
+  const fechaFormateadaEditada = fechaEditada.toLocaleDateString(
+    undefined,
+    opcionesFechaEditada
+  );
 
-/* tablaTareas.appendChild(tareasTabla); */
+  /* modifico la tarea */
+  agenda[indiceTarea].tarea = inputTarea.value;
+  agenda[indiceTarea].responsable = inputResponsable.value;
+  agenda[indiceTarea].urgencia = inputUrgencia.value;
+  agenda[indiceTarea].estado = `Editada`;
+  agenda[indiceTarea].fechaEdicion = fechaFormateadaEditada;
+
+  /* corrijo en pantalla los cambios */
+
+  const filaNueva = tbody.children[indiceTarea];
+  filaNueva.innerHTML = `
+    <td>${agenda[indiceTarea].tarea}</td>
+    <td>${agenda[indiceTarea].responsable}</td>
+    <td>${agenda[indiceTarea].urgencia}</td>
+    <td>Editada</td>
+    <td>
+      <button
+        type="button"
+        class="btn btn-primary me-1 btn-eliminar"
+        onclick="borrarTarea('${agenda[indiceTarea].id}')"
+      >
+        Eliminar
+      </button>
+      <button
+        type="button"
+        class="btn btn-success me-1 btn-editar"
+        onclick="prepararTarea('${agenda[indiceTarea].id}')"
+      >
+        Editar
+      </button>
+      <button type="button" class="btn btn-secondary btn-finalizar"
+      onclick="verTarea('${agenda[indiceTarea].id}')">
+        Ver más
+      </button>
+    </td>`;
+
+  guardarLocalstorage();
+  modalFormularioTarea.hide();
+};
+
+function limpiarFormulario() {
+  formularioTarea.reset();
+}
+
+/* ingresar filas en la tabla */
+
+const cargarTarea = () => {
+  console.log(agenda.length);
+  if (agenda.length !== 0) {
+    /* recorro la agenda y por cada elemento, dibujo una fila */
+    agenda.map((itemTarea, indice) => dibujarFila(itemTarea));
+  } else {
+    Swal.fire({
+      title: "La agenda esta vacia",
+      icon: "warning",
+      confirmButtonText: "OK",
+    });
+  }
+};
+
+const dibujarFila = (itemTarea) => {
+  tbody.innerHTML += ` <tr>
+ 
+                <td>${itemTarea.tarea}</td>
+                <td>${itemTarea.responsable}</td>
+                <td>${itemTarea.urgencia}</td>
+                <td>${`Original`}</td>
+                <td>
+                  <button
+                    type="button"
+                    class="btn btn-primary me-1 btn-eliminar" onclick="borrarTarea('${
+                      itemTarea.id
+                    }')"
+                  >
+                    Eliminar
+                  </button>
+                  <button type="button" class="btn btn-success me-1 btn-editar"onclick= "prepararTarea('${
+                    itemTarea.id
+                  }')">
+                    Editar
+                  </button>
+                  <button type="button" class="btn btn-secondary btn-finalizar" onclick="verTarea('${
+                    itemTarea.id
+                  }')">
+                    Ver más
+                  </button>
+                </td>
+              </tr>`;
+};
+
+/* borrar tareas con boton */
+
+window.borrarTarea = (id) => {
+  Swal.fire({
+    title: "Seguro quieres borrar la tarea?",
+    text: "No podras revertir este paso",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Borrar",
+    cancelButtonText: "cancelar",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      /* quiero encontrar la posicion del elemento que quiero borrar */
+      const indiceTarea = agenda.findIndex((tarea) => tarea.id == id);
+      console.log(indiceTarea);
+      console.log(tbody.children[indiceTarea]);
+
+      /* borro el elemento de esa posicion */
+      agenda.splice(indiceTarea, 1);
+      Swal.fire({
+        title: "OK!",
+        text: "Tu tarea ha sido borrada",
+        icon: "success",
+      });
+
+      /* borro en pantalla la fila eliminada */
+      tbody.children[indiceTarea].remove();
+
+      /* guardo el nuevo array sin la tarea borrada, en el localStorage */
+      guardarLocalstorage();
+    }
+  });
+};
+
+/* para editar una tarea */
+/* 1- traigo los elementos al formulario */
+/* encuentro el objeto */
+window.prepararTarea = (id) => {
+  const tareaBuscada = agenda.find((tarea) => tarea.id == id);
+  /* muestro los datos del objeto en el formulario */
+
+  inputTarea.value = tareaBuscada.tarea;
+  inputResponsable.value = tareaBuscada.responsable;
+  inputUrgencia.value = tareaBuscada.urgencia;
+  idTarea = id;
+  /* cambio la variable que controla si edito o creo */
+  estoyCreando = false;
+
+  modalFormularioTarea.show();
+};
+
+/* mostrar tabla de detalles */
+
+window.verTarea = (id) => {
+  const tarea = agenda.find((tarea) => tarea.id === id);
+  tablaDetalle.classList.remove("d-none");
+  detalleTareaTitulo.textContent = tarea.tarea;
+  detalleDescripcion.textContent = tarea.tarea;
+  detalleResponsable.textContent = tarea.responsable;
+  detalleEstado.textContent = tarea.estado;
+  detalleFechaEdit.textContent = tarea.fechaEdicion ?? "Sin editar";/* ??= "sino" */
+  detalleFechaCrea.textContent = tarea.fechaCrea;
+};
 
 //manejadores de eventos
 btnAgregarTarea.addEventListener("click", () => {
   modalFormularioTarea.show();
 });
+
+/* boton para cerrar tabla de detalles de tareas */
+btnVolverLista.addEventListener("click", () => {
+  tablaDetalle.classList.add("d-none");
+});
+
 /* cuando creo presiono submit, crear la tarea, vaciar el formulario y cerrar modal */
 formularioTarea.addEventListener("submit", (e) => {
   e.preventDefault();
+
   //aqui tengo que crear/editar una tarea
-  crearTarea();
+
+  if (estoyCreando == true) {
+    crearTarea();
+  } else {
+    editarTarea();
+
+    estoyCreando = true;
+    idTarea = null;
+  }
+
   formularioTarea.reset();
   modalFormularioTarea.hide();
 });
+
+cargarTarea();
--- JS/tarea.js
+++ JS/tarea.js
@@ -2,25 +2,28 @@ export default class Tarea {
   /* propiedades */
   #id;
   #tarea;
-  #fecha;
+  #fechaEdicion;
   #estado;
   #responsable;
   #urgencia;
+  #fechaCrea;
 
   constructor(
     /* parametros */
     tarea,
-    fecha,
     estado,
     responsable,
-    urgencia
+    urgencia,
+    fechaCrea,
+    fechaEdicion
   ) {
     this.#id = crypto.randomUUID();
     this.#tarea = tarea;
-    this.#fecha = fecha;
-    this.#estado = `creada`;
+    this.#estado = estado;
     this.#responsable = responsable;
     this.#urgencia = urgencia;
+    this.#fechaCrea = fechaCrea;
+    this.#fechaEdicion = fechaEdicion;
   }
 
   // Getters
@@ -32,8 +35,8 @@ export default class Tarea {
     return this.#tarea;
   }
 
-  get fecha() {
-    return this.#fecha;
+  get fechaEdicion() {
+    return this.#fechaEdicion;
   }
 
   get estado() {
@@ -45,6 +48,9 @@ export default class Tarea {
   get urgencia() {
     return this.#urgencia;
   }
+  get fechaCrea() {
+    return this.#fechaCrea;
+  }
 
   // Setters
   set id(nuevoId) {
@@ -55,8 +61,8 @@ export default class Tarea {
     this.#tarea = nuevaTarea;
   }
 
-  set fecha(nuevaFecha) {
-    this.#fecha = nuevaFecha;
+  set fechaEdicion(nuevafechaEdicion) {
+    this.#fechaEdicion = nuevafechaEdicion;
   }
 
   set estado(nuevoEstado) {
@@ -68,16 +74,20 @@ export default class Tarea {
   set urgencia(nuevoUrgencia) {
     this.#urgencia = nuevoUrgencia;
   }
+  set fechaCrea(nuevafechaCrea) {
+    this.#fechaCrea = nuevafechaCrea;
+  }
 
   //    metodo para almacenar el objeto en el localstorage/sessionstorage
   toJSON() {
     return {
       id: this.id,
       tarea: this.tarea,
-      fecha: this.fecha,
+      fechaEdicion: this.fechaEdicion,
       estado: this.estado,
       responsable: this.responsable,
       urgencia: this.#urgencia,
+      fechaCrea: this.#fechaCrea,
     };
   }
 }
